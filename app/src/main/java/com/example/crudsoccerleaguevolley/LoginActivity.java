afdfe12d8d4514d7eb8931f62b2da06310a8273b
spainchesterdaeg <spainchester@outlook.com>
2020-11-23 19:08:47 -0600

    login implementado

---
+++ app/src/main/java/com/example/crudsoccerleaguevolley/LoginActivity.java
@@ -0,0 +1,134 @@
+
+package com.example.crudsoccerleaguevolley;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText username,password;
+    private Button btn_login;
+    private TextView link_regist;
+    private ProgressBar loading;
+    private static String URL_LOGIN = "https://chestersports.000webhostapp.com/login.php";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loading = findViewById(R.id.loading);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        btn_login = findViewById(R.id.btn_login);
+        link_regist = findViewById(R.id.link_regist);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mUsername = username.getText().toString().trim();
+                String mPass = password.getText().toString().trim();
+
+                if(!mUsername.isEmpty() || !mPass.isEmpty()){
+                    Login(mUsername,mPass);
+                } else {
+                    username.setError("Please insert email");
+                    password.setError("Please insert password");
+                }
+            }
+        });
+     link_regist.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+         }
+     });
+    }
+
+    private void Login(String username,String password){
+        loading.setVisibility(View.VISIBLE);
+        btn_login.setVisibility(View.GONE);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_LOGIN, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response.substring(response.indexOf("{"),response.lastIndexOf("}")+1));
+                    String success = jsonObject.getString("success");
+                    JSONArray jsonArray = jsonObject.getJSONArray("login");
+
+                    if(success.equals("1")){
+                        for(int i = 0; i < jsonArray.length();i++){
+                            JSONObject object = jsonArray.getJSONObject(i);
+                            String username = object.getString("username").trim();
+                            String email = object.getString("email").trim();
+
+                            Toast.makeText(LoginActivity.this,"Success Login. \n Your USERNAME:"+username
+                                    +"\nYour Email : "+email,Toast.LENGTH_SHORT).show();
+
+                            loading.setVisibility(View.GONE);
+
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    loading.setVisibility(View.GONE);
+                    btn_login.setVisibility(View.VISIBLE);
+                    Toast.makeText(LoginActivity.this,"Exception "+e.toString(),Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        loading.setVisibility(View.GONE);
+                        btn_login.setVisibility(View.VISIBLE);
+                        Toast.makeText(LoginActivity.this,"Error "+error.toString(),Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("username",username);
+                params.put("password",password);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+
+
+    }
+}
