cfc05ffb9910bc9f5e91e3105567b5ed5afc3169
spainchesterdaeg <spainchester@outlook.com>
2020-12-07 13:02:27 -0600

    poder editar foto

--- app/src/main/java/com/example/crudsoccerleaguevolley/HomeActivity.java
+++ app/src/main/java/com/example/crudsoccerleaguevolley/HomeActivity.java
@@ -1,12 +1,17 @@
 package com.example.crudsoccerleaguevolley;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -30,18 +35,24 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class HomeActivity extends AppCompatActivity {
     private static final String TAG = HomeActivity.class.getSimpleName();
     private TextView email,username;
-    private Button btn_logout;
+    private Button btn_logout,btn_photo_upload;
     SessionManager sessionManager;
     String getId_User;
     private static String URL_READ =  "https://chestersports.000webhostapp.com/read_detail.php";
     private static String URL_EDIT =  "https://chestersports.000webhostapp.com/edit_detail.php";
-
+    private static String URL_UPLOAD =  "https://chestersports.000webhostapp.com/upload.php";
+    private Bitmap bitmap;
+    CircleImageView profile_image;
     private Menu action;
 
 
@@ -56,6 +67,8 @@ public class HomeActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
         username = findViewById(R.id.username);
         btn_logout = findViewById(R.id.btn_logout);
+        btn_photo_upload = findViewById(R.id.btn_photo);
+        profile_image = findViewById(R.id.profile_image);
 
         HashMap<String,String> user = sessionManager.getUserDetail();
         getId_User = user.get(sessionManager.ID_USER);
@@ -76,6 +89,14 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 sessionManager.logout();
+            }
+        });
+
+        btn_photo_upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                chooseFile();
             }
         });
     }
@@ -248,4 +269,86 @@ public class HomeActivity extends AppCompatActivity {
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
     }
+
+    private void chooseFile(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select Picture"),1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null){
+            Uri filePath = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),filePath);
+                profile_image.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            UploadPicture(getId_User,getStringImage(bitmap));
+
+        }
+    }
+
+    private void UploadPicture(final String id_user,final String photo){
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Uploading...");
+        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_UPLOAD, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                progressDialog.dismiss();
+                Log.i(TAG,response.toString());
+                try {
+                    JSONObject jsonObject = new JSONObject(response.substring(response.indexOf("{"),response.lastIndexOf("}")+1));
+                    String success = jsonObject.getString("success");
+                    if(success.equals("1")){
+                        Toast.makeText(HomeActivity.this,"success",Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    progressDialog.dismiss();
+                    Toast.makeText(HomeActivity.this,"TRY AGAIN!"+e.toString(),Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.dismiss();
+                Toast.makeText(HomeActivity.this,"ERROR"+error.toString(),Toast.LENGTH_SHORT).show();
+            }
+        })
+
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("id_user",id_user);
+                params.put("photo",photo);
+                return params;
+            }
+        } ;
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    public String getStringImage(Bitmap bitmap){
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,byteArrayOutputStream);
+
+        byte[] imageByteArray = byteArrayOutputStream.toByteArray();
+        String encodedImage = Base64.encodeToString(imageByteArray,Base64.DEFAULT);
+        return encodedImage;
+    }
+
+
+
 }
